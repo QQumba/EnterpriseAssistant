5727c5a01750f7e4ae6ca67332ff8d38b9510157
Nick Knysh <46058617+QQumba@users.noreply.github.com>
2022-01-19 15:58:23 +0200

    model(department): create department table scripts

---
+++ EnterpriseAssistant.Db/Tables/table_department.sql
@@ -0,0 +1,11 @@
+﻿create table if not exists "department"
+(
+    id                  bigserial not null,
+    chief_department_id bigint    null,
+    name                varchar(250),
+    constraint pk_department_id
+        primary key (id),
+    constraint fk_department_chief_department_id
+        foreign key (chief_department_id)
+            references "department" (id)
+);

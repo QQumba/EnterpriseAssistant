93e5fac7d9ddc6cc18bf8435a8f48aa474fc7477
Nick Knysh <46058617+QQumba@users.noreply.github.com>
2022-02-11 18:48:53 +0200

    create basic layout

--- EnterpriseAssistant.UI/src/app/app.module.ts
+++ EnterpriseAssistant.UI/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [AppComponent, SidebarComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]

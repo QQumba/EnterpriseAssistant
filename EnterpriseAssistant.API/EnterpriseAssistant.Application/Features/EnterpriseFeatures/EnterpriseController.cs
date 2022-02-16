8b8189bc444fa7eaffe05f44da1abd2a84009418
Nick Knysh <46058617+QQumba@users.noreply.github.com>
2022-02-16 19:11:26 +0200

    feature: create enterprise

--- EnterpriseAssistant.API/EnterpriseAssistant.Application/Features/EnterpriseFeatures/EnterpriseController.cs
+++ EnterpriseAssistant.API/EnterpriseAssistant.Application/Features/EnterpriseFeatures/EnterpriseController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Reflection.Metadata;
 using EnterpriseAssistant.Application.Features.EnterpriseFeatures.Commands;
+using EnterpriseAssistant.Application.Features.EnterpriseFeatures.ViewModels;
 using EnterpriseAssistant.Application.Features.UserFeatures.ViewModels;
+using EnterpriseAssistant.DataAccess.Entities;
+using Mapster;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -24,5 +27,14 @@ public class EnterpriseController : ControllerBase
         var result = await _mediator.Send(new CreateEnterpriseUser());
 
         return Ok();
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<EnterpriseViewModel>> CreateEnterprise([FromBody] EnterpriseCreateViewModel model)
+    {
+        var result = await _mediator.Send(new CreateEnterprise(model));
+
+
+        return result.Match(Ok);
     }
 }

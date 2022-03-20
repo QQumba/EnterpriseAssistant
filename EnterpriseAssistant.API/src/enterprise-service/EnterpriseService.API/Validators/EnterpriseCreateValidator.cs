df8ac810c49217b93ebbdec3a23b082d5d95bd0a
Nick Knysh <46058617+QQumba@users.noreply.github.com>
2022-03-20 21:11:26 +0200

    Update EnterpriseCreateValidator.cs

--- EnterpriseAssistant.API/src/enterprise-service/EnterpriseService.API/Validators/EnterpriseCreateValidator.cs
+++ EnterpriseAssistant.API/src/enterprise-service/EnterpriseService.API/Validators/EnterpriseCreateValidator.cs
@@ -3,11 +3,12 @@ using FluentValidation;
 
 namespace EnterpriseService.API.Validators;
 
-public class EnterpriseCreateValidator :  AbstractValidator<EnterpriseCreateViewModel>
+public class EnterpriseCreateValidator : AbstractValidator<EnterpriseCreateViewModel>
 {
     public EnterpriseCreateValidator()
     {
-        
+        RuleFor(e => e.Id).MaximumLength(50)
+            .WithMessage(e => $"Max enterprise id length is 50, provided id length: {e.Id.Length}");
         RuleFor(e => e.UserCreate).InjectValidator();
         RuleFor(e => e.DepartmentCreate).InjectValidator();
     }

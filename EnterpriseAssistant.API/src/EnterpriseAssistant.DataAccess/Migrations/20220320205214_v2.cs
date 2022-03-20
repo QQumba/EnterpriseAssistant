c473b72936543e41bacdc6e6cb3641f89495e72a
Nick Knysh <46058617+QQumba@users.noreply.github.com>
2022-03-20 22:53:11 +0200

    add migration

---
+++ EnterpriseAssistant.API/src/EnterpriseAssistant.DataAccess/Migrations/20220320205214_v2.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnterpriseAssistant.DataAccess.Migrations
+{
+    public partial class v2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "enterprise_id",
+                table: "user",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_enterprise_id",
+                table: "user",
+                column: "enterprise_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_user_enterprise_enterprise_id",
+                table: "user",
+                column: "enterprise_id",
+                principalTable: "enterprise",
+                principalColumn: "id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_user_enterprise_enterprise_id",
+                table: "user");
+
+            migrationBuilder.DropIndex(
+                name: "IX_user_enterprise_id",
+                table: "user");
+
+            migrationBuilder.DropColumn(
+                name: "enterprise_id",
+                table: "user");
+        }
+    }
+}

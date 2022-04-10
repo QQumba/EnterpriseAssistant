c51a6454143c7c2b8518a94312ef2efcf8aba829
Nikita Knysh <46058617+QQumba@users.noreply.github.com>
2022-04-10 15:26:58 +0300

    update model

--- EnterpriseAssistant.API/src/EnterpriseAssistant.DataAccess/Entities/Department.cs
+++ EnterpriseAssistant.API/src/EnterpriseAssistant.DataAccess/Entities/Department.cs
@@ -4,17 +4,18 @@ namespace EnterpriseAssistant.DataAccess.Entities;
 
 public class Department : BaseEntity.WithId<long>
 {
-    public string Name { get; set; }
+    public string Name { get; set; } = null!;
 
     public long? ParentDepartmentId { get; set; }
 
-    public Department ParentDepartment { get; set; }
+    public Department? ParentDepartment { get; set; }
 
-    public ICollection<Department> ChildDepartments { get; set; }
+    // TODO: check if no child departments exist, is this null or empty?
+    public ICollection<Department>? ChildDepartments { get; set; }
 
     public DepartmentType DepartmentType { get; set; } = DepartmentType.Default;
 
-    public string EnterpriseId { get; set; }
+    public string EnterpriseId { get; set; } = null!;
 
-    public Enterprise Enterprise { get; set; }
+    public Enterprise? Enterprise { get; set; }
 }

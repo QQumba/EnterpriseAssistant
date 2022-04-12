8795c12007f7fda9a3af1b207bd8374e5646c4c3
Nikita Knysh <46058617+QQumba@users.noreply.github.com>
2022-04-12 21:08:50 +0300

    add column names

--- EnterpriseAssistant.API/src/EnterpriseAssistant.DataAccess/Entities/BaseEntity.cs
+++ EnterpriseAssistant.API/src/EnterpriseAssistant.DataAccess/Entities/BaseEntity.cs
@@ -1,15 +1,21 @@
-﻿namespace EnterpriseAssistant.DataAccess.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EnterpriseAssistant.DataAccess.Entities
 {
     public abstract class BaseEntity
     {
+        [Column("created_at")]
         public DateTime CreatedAt { get; set; }
 
+        [Column("updated_at")]
         public DateTime UpdatedAt { get; set; }
 
+        [Column("is_soft_deleted")]
         public bool IsSoftDeleted { get; set; }
 
         public abstract class WithId<T> : BaseEntity
         {
+            [Column("id")]
             public T Id { get; set; }
         }
     }

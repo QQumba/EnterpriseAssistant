fb7ab0ff51dcb5aa200fb41b6b8dcd07c601a70e
Nikita Knysh <46058617+QQumba@users.noreply.github.com>
2022-04-12 21:09:40 +0300

    remove base entity names configuration

--- EnterpriseAssistant.API/src/EnterpriseAssistant.DataAccess/Configurations/BaseEntityEntityTypeBuilderExtensions.cs
+++ EnterpriseAssistant.API/src/EnterpriseAssistant.DataAccess/Configurations/BaseEntityEntityTypeBuilderExtensions.cs
@@ -25,12 +25,4 @@ public static class BaseEntityEntityTypeBuilderExtensions
         builder.HasKey(e => e.Id);
         builder.Property(e => e.Id).HasColumnName("id").ValueGeneratedOnAdd();
     }
-
-    public static EntityTypeBuilder<T> ConfigureBaseEntity<T>(this EntityTypeBuilder<T> builder) where T : BaseEntity
-    {
-        builder.Property(e => e.CreatedAt).HasColumnName("created_at").IsRequired();
-        builder.Property(e => e.UpdatedAt).HasColumnName("updated_at").IsRequired();
-        builder.Property(e => e.IsSoftDeleted).HasColumnName("is_soft_deleted").IsRequired();
-        return builder;
-    }
 }

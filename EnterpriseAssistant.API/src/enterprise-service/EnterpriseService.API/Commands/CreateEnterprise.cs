cf6a1b664fa7cbadece06023d52fbd7065399a0c
Nikita Knysh <46058617+QQumba@users.noreply.github.com>
2022-04-18 00:01:49 +0300

    create enterprise refactoring

--- EnterpriseAssistant.API/src/enterprise-service/EnterpriseService.API/Commands/CreateEnterprise.cs
+++ EnterpriseAssistant.API/src/enterprise-service/EnterpriseService.API/Commands/CreateEnterprise.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using DepartmentService.Contract.ViewModels;
 using EnterpriseAssistant.DataAccess;
 using EnterpriseAssistant.DataAccess.Entities;
 using EnterpriseAssistant.DataAccess.Entities.Enums;
@@ -38,40 +39,56 @@ public class CreateEnterpriseHandler
     public async Task<OneOf<EnterpriseViewModel, EnterpriseIdAlreadyTakenError>> Handle(CreateEnterprise request,
         CancellationToken cancellationToken)
     {
-        // create new enterprise
         if (await _db.Enterprises.IsIdTaken(request.EnterpriseCreate.Id, cancellationToken))
         {
             return new EnterpriseIdAlreadyTakenError(request.EnterpriseCreate.Id);
         }
-        var enterpriseToCreate = request.EnterpriseCreate.Adapt<Enterprise>();
-        
-        enterpriseToCreate.OwnerEmail = request.OwnerEmail;
-        var enterprise = _db.Enterprises.Add(enterpriseToCreate).Entity;
 
-        // create new department and add to enterprise
-        var departmentToCreate = request.EnterpriseCreate.DepartmentCreate.Adapt<Department>();
-        departmentToCreate.DepartmentType = DepartmentType.Root;
-        // todo: check if department id set to departmentToCreate.Id
-        departmentToCreate.Enterprise = enterpriseToCreate;
-        _db.Departments.Add(departmentToCreate);
+        var enterprise = CreateEnterprise(request.EnterpriseCreate, request.OwnerEmail);
 
-        // create new user and add to enterprise and to root department
-        var userToCreate = request.EnterpriseCreate.UserCreate.Adapt<User>();
-        userToCreate.Salt = "";
-        userToCreate.ManagedUserEmail = request.OwnerEmail;
-        userToCreate.Enterprise = enterpriseToCreate;
-        _db.Users.Add(userToCreate);
-        var departmentUser = new DepartmentUser
-        {
-            Department = departmentToCreate,
-            User = userToCreate,
-            DepartmentUserRole = DepartmentUserRole.Admin,
-            EnterpriseId = enterpriseToCreate.Id
-        };
+        var department = CreateDepartment(request.EnterpriseCreate.DepartmentCreate);
+        department.Enterprise = enterprise;
 
-        _db.DepartmentUsers.Add(departmentUser);
+        CreateUserForDepartment(request.EnterpriseCreate.UserCreate.Adapt<User>(), department);
 
         await _db.SaveChangesAsync(cancellationToken);
         return enterprise.Adapt<EnterpriseViewModel>();
+    }
+
+    private Enterprise CreateEnterprise(
+        EnterpriseCreateViewModel enterpriseCreate, string email)
+    {
+        var enterpriseToCreate = enterpriseCreate.Adapt<Enterprise>();
+
+        enterpriseToCreate.OwnerEmail = email;
+        var enterprise = _db.Enterprises.Add(enterpriseToCreate).Entity;
+
+        return enterprise;
+    }
+
+    private Department CreateDepartment(DepartmentCreateViewModel departmentCreate)
+    {
+        var departmentToCreate = departmentCreate.Adapt<Department>();
+        departmentToCreate.DepartmentType = DepartmentType.Root;
+        return _db.Departments.Add(departmentToCreate).Entity;
+    }
+
+    private void CreateUserForDepartment(User user, Department department)
+    {
+        var enterprise = department.Enterprise!;
+        // todo: add password secret handling
+        user.Salt = "default_salt";
+        user.ManagedUserEmail = enterprise.OwnerEmail;
+        user.Enterprise = enterprise;
+        _db.Users.Add(user);
+        var departmentUser = new DepartmentUser
+        {
+            Department = department,
+            User = user,
+            DepartmentUserRole = DepartmentUserRole.Admin,
+            EnterpriseId = enterprise.Id
+        };
+
+        _db.DepartmentUsers.Add(departmentUser);
     }
 }

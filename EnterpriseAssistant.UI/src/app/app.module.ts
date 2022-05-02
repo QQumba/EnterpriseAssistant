fa34efd038927fcb0a398f95e22a6fed54ea38cc
Nikita Knysh <46058617+QQumba@users.noreply.github.com>
2022-05-02 17:21:08 +0300

    add i18n

--- EnterpriseAssistant.UI/src/app/app.module.ts
+++ EnterpriseAssistant.UI/src/app/app.module.ts
@@ -5,10 +5,60 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
+import { ManagedUserCreateComponent } from './components/managed-user/managed-user-create/managed-user-create.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LoginComponent } from './components/login/login.component';
+import { EnterpriseSidebarEnterprisePickerComponent } from './components/enterprise/enterprise-sidebar-enterprise-picker/enterprise-sidebar-enterprise-picker.component';
+import { EnterpriseSidebarMenuComponent } from './components/enterprise/enterprise-sidebar-menu/enterprise-sidebar-menu.component';
+import { DepartmentSidebarMenuComponent } from './components/department/department-sidebar/department-sidebar-menu.component';
+import { SidebarMenuActionComponent } from './components/sidebar/sidebar-menu-action/sidebar-menu-action.component';
+import { EnterpriseCreateEnterpriseComponent } from './components/enterprise/enterprise-create-enterprise/enterprise-create-enterprise.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoaderComponent } from './components/utilities/loader/loader.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { EnterpriseUsersComponent } from './components/enterprise/enterprise-users/enterprise-users.component';
+import { EnterpriseUsersCreateUserComponent } from './components/enterprise/enterprise-users/enterprise-users-create-user/enterprise-users-create-user.component';
+import { FormSegmentComponent } from './components/utilities/form-segment/form-segment.component';
+import { FormInputComponent } from './components/utilities/form-input/form-input.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
-  declarations: [AppComponent, SidebarComponent, NavbarComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    ManagedUserCreateComponent,
+    LoginComponent,
+    DepartmentSidebarMenuComponent,
+    EnterpriseSidebarEnterprisePickerComponent,
+    EnterpriseSidebarMenuComponent,
+    SidebarMenuActionComponent,
+    EnterpriseCreateEnterpriseComponent,
+    LoaderComponent,
+    EnterpriseUsersComponent,
+    EnterpriseUsersCreateUserComponent,
+    FormSegmentComponent,
+    FormInputComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })

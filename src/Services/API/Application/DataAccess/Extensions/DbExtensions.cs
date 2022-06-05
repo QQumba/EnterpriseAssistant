ffbad33212b4ac92645e81abb6ab72b4ecc47c75
Nikita Knysh <46058617+QQumba@users.noreply.github.com>
2022-06-05 17:07:16 +0300

    create add department chief extension

---
+++ src/Services/API/Application/DataAccess/Extensions/DbExtensions.cs
@@ -0,0 +1,23 @@
+﻿using EnterpriseAssistant.Application.Shared;
+using EnterpriseAssistant.DataAccess.Entities;
+using EnterpriseAssistant.DataAccess.Entities.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnterpriseAssistant.DataAccess.Extensions;
+
+public static class DbExtensions
+{
+    public static async Task<User?> GetDepartmentChiefUser(this EnterpriseAssistantDbContext context,
+        AuthContext authContext)
+    {
+        return await context.DepartmentUsers
+            .Where(du => du.EnterpriseId.Equals(authContext.EnterpriseId)
+                         && du.IsSoftDeleted == false
+                         && du.UserId == authContext.UserId
+                         && (du.DepartmentUserRole == DepartmentUserRole.Chief ||
+                             du.DepartmentUserRole == DepartmentUserRole.Admin))
+            .Include(du => du.User)
+            .Select(du => du.User)
+            .FirstOrDefaultAsync(d => d.IsSoftDeleted == false);
+    }
+}

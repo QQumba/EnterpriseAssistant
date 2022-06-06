3f4c39eb9eaa00880dab85761bdcf382d21d0d55
Nikita Knysh <46058617+QQumba@users.noreply.github.com>
2022-06-06 18:40:48 +0300

    add invite migration

--- src/Services/API/Application/DataAccess/Migrations/20220606154006_add_invites.Designer.cs
+++ src/Services/API/Application/DataAccess/Migrations/20220606154006_add_invites.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EnterpriseAssistant.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EnterpriseAssistant.DataAccess.Migrations
 {
     [DbContext(typeof(EnterpriseAssistantDbContext))]
-    partial class EnterpriseAssistantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220606154006_add_invites")]
+    partial class add_invites
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

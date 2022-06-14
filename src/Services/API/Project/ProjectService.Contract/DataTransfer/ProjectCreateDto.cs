951db1f268dd228faa30b28947f797256cfd074e
Nikita Knysh <46058617+QQumba@users.noreply.github.com>
2022-06-14 11:33:09 +0300

    small fixes

--- src/Services/API/Project/ProjectService.Contract/DataTransfer/ProjectCreateDto.cs
+++ src/Services/API/Project/ProjectService.Contract/DataTransfer/ProjectCreateDto.cs
@@ -5,7 +5,8 @@ namespace ProjectService.Contract.DataTransfer;
 public class ProjectCreateDto
 {
     public string Name { get; set; } = null!;
-    public string Description { get; set; } = String.Empty;
-    public string EnterpriseId { get; set; } = null!;
+
+    public string Description { get; set; } = string.Empty;
+
     public DepartmentCreateDto DepartmentCreate { get; set; } = null!;
 };
